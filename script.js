3a3c4a5a8dd07afd0bdfc4081c8d56011b76b693
Owais <zaidalit00@gmail.com>
2024-04-21 15:56:48 +0530

    tweaked some of the css and html and wrote js logic according to the new rules

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ const questions = [
     { jumbledWord: "betal", answer: "table" }
 ];
 
+
+//Variables
 let current = 0; // Current question index
+let tries = 5; // Number of maximum tries
+let result = "";
+
 
 // DOM elements
 let scrambledQuestion = document.querySelector(".ques-container");
 let inputs = document.querySelectorAll("input");
+let remainingTries = document.querySelector(".remaining-tries");
+
+
+//Buttons
+let resetBtn = document.querySelector(".reset");
+let guessBtn = document.querySelector(".guess");
+
 
 // Show the current question
 const showQuestion = () => {
@@ -16,40 +28,36 @@ const showQuestion = () => {
 };
 showQuestion();
 
-// Next and previous input selection logic.
+
+//Showing the remaining tries.
+remainingTries.innerText = tries;
+
+
 inputs.forEach((inp, index) => {
 
-    inp.addEventListener("keyup", (event) => {
+    inp.addEventListener("input", (event) => {
         const valueLength = inp.value.length;
+        const value = inp.value;
 
         // Entered a character and not the last field
         if (valueLength === 1 && index < inputs.length - 1) {
-            inputs[index + 1].focus(); 
+            inputs[index + 1].focus();
+            result += value; // Append the entire input value to result
+            inp.disabled = true;
         }
 
-        // Button navigation function.
-        navigate()
-
+        // Entered a character in the last field
+        else if (valueLength === 1 && index === inputs.length - 1) {
+            result += value;
+            inp.disabled = true;           
+        }
 
     });
+
 });
 
-//Function to navigate the input fields through buttons.
-const navigate = () => {
 
-    inputs.forEach((inp, index) => {
-        inp.addEventListener("keyup", (event) => {
-
-            ; if (event.key === "Backspace" && index > 0) {
-                inputs[index - 1].focus();
-
-            } else if (event.key === "ArrowRight" && index < (inputs.length - 1)) {
-                inputs[index + 1].focus();
-
-            } else if (event.key === "LeftArrow" && index > 0) {
-                inputs[index - 1].focus()
-            }
-        });
-    });
-
-}
+// Reset button functionality
+resetBtn.addEventListener("click", () => {
+    window.location.reload(true);
+});
